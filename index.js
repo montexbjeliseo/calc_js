75c0cdaa4d28995d743e0562a8d4e4e885065daa
ees28ar <ees28.ar@gmail.com>
2023-05-18 22:13:31 -0300

    cambios

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const app = document.getElementById("app");
 
-app.innerHTML = "<h1>Hello world!</h1>";
+function crearCalculadora (){
+    const calculadora = document.createElement('div');
+    calculadora.classList.add('calculadora');
+    app.appendChild(calculadora);
+    calculadora.appendChild(crearPanelSuperior());
+}
+function crearPanelSuperior(){
+    const panelSuperior= document.createElement('div');
+    panelSuperior.appendChild(crearPantalla());
+    return panelSuperior;
+}
+
+function crearPantalla(){
+    const pantalla= document.createElement('div');
+    pantalla.classList.add('pantalla');
+    return pantalla;
+}
+
+
+
+crearCalculadora();
+
+
+
